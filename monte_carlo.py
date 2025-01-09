15c41780f5b25a8f9fa28a2b56bdb94d66e89e1e
gianeh <pietropianigiani2@gmail.com>
2025-01-09 15:35:21 +0100

    monte carlo

--- monte_carlo.py
+++ monte_carlo.py
@@ -10,10 +10,10 @@ game_powers = [0,2,4,6,8,10]
 simulations_number = 100
 
 enumerator = States_enumerator(map_filename="ez_map", logging=True)
-value_iterator = Value_iterator(enumerator, alpha=0.9, delta=0.01, epsilon=1, lose_cost=20000, win_cost=-10000, move_cost=10, eat_cost=-1e1, power=10, control_accuracy=True, logging=True)
-#value_iterator.run()
-#value_iterator.store_policy()
-#value_iterator.store_value_function()
+value_iterator = Value_iterator(enumerator, alpha = 0.9, epsilon = 1, lose_cost = 20000, win_cost = -50000, move_cost = 10, eat_cost = -10, power = 10, control_accuracy=True, logging=True)
+value_iterator.run()
+value_iterator.store_policy()
+value_iterator.store_value_function()
 
 manager = Manager()
 progress_dict = manager.dict()
